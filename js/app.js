e6d1ba51e49045dfc077c835be1f3082322f15a3
nigirock <nigirock124@gmail.com>
2015-11-20 09:54:00 +0300

    final

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-var app = angular.module("APP",["ngRoute"]);
+var app = angular.module("APP",['ngRoute','ngAnimate']);
 app.controller("mainCtrl",function($scope,$http,$location){
     $http.get("data.json").success(function(data){
         $scope.tableDataTechnology = data.tableTechnology;
         $scope.tableDataSales = data.tableSales;
+        $scope.tableDataMarketing = data.tableMarketing;
         $location.path("/index.html");
+        $scope.goToTechnology();
     });
 });
 app.config(function($routeProvider,$locationProvider){
@@ -15,10 +17,27 @@ app.config(function($routeProvider,$locationProvider){
     $routeProvider.when("/technology",{
         templateUrl:"/project_manager/technology.html"
     });
+    $routeProvider.when("/technology_2",{
+        templateUrl:"/project_manager/technology_2.html"
+    });
     $routeProvider.when("/sales",{
         templateUrl:"/project_manager/sales.html"
+    });
+    $routeProvider.when("/sales_2",{
+        templateUrl:"/project_manager/sales_2.html"
+    });
+    $routeProvider.when("/marketing",{
+        templateUrl:"/project_manager/marketing.html"
+    });
+    $routeProvider.when("/marketing_2",{
+        templateUrl:"/project_manager/marketing_2.html"
     });
     $routeProvider.otherwise({
         redirectTo:"/"
     });
+});
+app.filter('slice', function() {
+    return function(arr, start, end) {
+        return arr.slice(start, end);
+    };
 });
--- js/directives.js
+++ js/directives.js
@@ -1,11 +1,28 @@
 app.directive("ngButtonRow",function($location){
     return{
-        link: function(scope){
+        link: function(scope,element){
+            console.log(element.find("button")[0])
+            var btn0 = element.find("button")[0];
+            $(btn0).addClass('active');
             scope.goToTechnology = function(){
+                $(scope.btn1).addClass('active_btn');
+                $(scope.btn2).removeClass('active_btn');
                 $location.path("/technology");
+                scope.path = $location.path("/technology").$$path;
            };
             scope.goToSales = function(){
+                $(btn0).removeClass('active');
+                $(scope.btn1).addClass('active_btn');
+                $(scope.btn2).removeClass('active_btn');
                 $location.path("/sales");
+                scope.path = $location.path("/sales").$$path;
+            };
+            scope.goToMarketing = function(){
+                $(btn0).removeClass('active');
+                $(scope.btn1).addClass('active_btn');
+                $(scope.btn2).removeClass('active_btn');
+                $location.path("/marketing");
+                scope.path = $location.path("/marketing").$$path;
             };
         },
         restrict: "A",
@@ -15,22 +32,15 @@ app.directive("ngButtonRow",function($location){
 app.directive('ngActive', function() {
     return {
         link: function(scope, element) {
-            element.bind('click', function() {
+            element.on('click', function() {
                 element.addClass('active');
+                element.removeClass('active');
             });
         },
         restrict: "A"
     }
 });
-app.directive("ngTechnology",function(){
-    return{
-        link: function(scope,element,attributes){
 
-        },
-        restrict: "A",
-        templateUrl: "technology.html"
-    }
-});
 app.directive("ngBlock",function(){
     return{
         link: function(scope,element,attributes){
@@ -40,14 +50,38 @@ app.directive("ngBlock",function(){
         templateUrl: "block_main.html"
     }
 });
-app.directive("ngPage",function(){
+app.directive("ngPage",function($location){
     return{
         link: function(scope,element) {
-            scope.prePage = function () {
+            var btn1 = element.children()[0];
+            var btn2 = element.children()[1];
+            scope.btn1 = btn1;
+            scope.btn2 = btn2;
+            $(btn1).addClass('active_btn');
 
+            scope.prePage = function () {
+                    $(btn1).addClass('active_btn');
+                    $(btn2).removeClass('active_btn');
+
+                    if(scope.path == "/technology"){
+                        $location.path("/technology");
+                    }else if(scope.path == "/sales"){
+                        $location.path("/sales");
+                    }else if(scope.path == "/marketing"){
+                        $location.path("/marketing");
+                    }
             };
             scope.nextPage = function () {
+                    $(btn1).removeClass('active_btn');
+                    $(btn2).addClass('active_btn');
 
+                    if(scope.path == "/technology"){
+                        $location.path("/technology_2");
+                    }else if(scope.path  == "/sales"){
+                        $location.path("/sales_2");
+                    }else if(scope.path == "/marketing"){
+                        $location.path("/marketing_2");
+                    }
             };
         },
         restrict: "A"
